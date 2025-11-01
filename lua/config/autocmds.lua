6bb72af800c138f586ef9ee552a2626d277fd02e
Alberto Rivadulla <albertorivadullasanchez@gmail.com>
2025-11-01 18:23:40 +0100

    Add an autocommand to use tabs instead of spaces on bash files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -71,3 +71,13 @@ vim.api.nvim_create_autocmd("FileType", {
 -- 		}
 -- 	end,
 -- })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "sh", "bash" },
+  -- command = "setlocal noexpandtab",
+  callback = function()
+    vim.opt_local.expandtab = false
+    -- vim.opt_local.shiftwidth = 4
+    -- vim.opt_local.tabstop = 4
+  end,
+})
